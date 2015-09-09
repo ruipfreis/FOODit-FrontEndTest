1c911db6b063ace8e0d5a65159d4f49d799baa60
Rui Reis <ruipfreis@gmail.com>
2015-09-09 03:51:01 +0100

    +added some simple animations

--- app/scripts/directives/headerbar.js
+++ app/scripts/directives/headerbar.js
@@ -7,22 +7,10 @@
  * # headerbar
  */
 angular.module('jstestApp')
-  .directive('headerBar', function (OrderService) {
+  .directive('headerBar', function () {
     return {
       templateUrl: 'views/header-bar.html',
       restrict: 'AE',
-      controller: 'MainCtrl',
-      link: function (scope, element, attrs) {
-
-        //Todo: should handle with a drag event later
-
-        angular.element(document.getElementsByClassName('order-summary')).bind('click', function(event){
-          if(element.hasClass('opened')) {
-            element.removeClass('opened').addClass('closed');
-          } else {
-            element.removeClass('closed').addClass('opened');
-          }
-        });
-      }
+      controller: 'MainCtrl'
     };
   });
